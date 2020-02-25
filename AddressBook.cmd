45dcf328afaa92809e12d3031e4c8a5a662b4dfe
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2020-02-24 19:44:02 -0600

    Deleting Old Files

--- AddressBook.cmd
+++
@@ -1,6 +0,0 @@
-@ECHO off
-title Address Book
-cls
-java -jar "%~dp0Address Book.jar"
-echo Thank you for using the Address Book using MySQL
-pause
--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"<Unnamed>","l":"Address"},{"p":"<Unnamed>","l":"addressBookMain"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Occupation"},{"p":"<Unnamed>","l":"Person"}]
